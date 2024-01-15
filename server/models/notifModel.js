c4c5c53dcfbf01c1c4ff42c28d6dca3007d0fc51
Khushaal Sharma <khushaalsharma1@gmail.com>
2024-01-15 19:06:14 +0530

    notification apis

--- server/models/notifModel.js
+++ server/models/notifModel.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+const dataSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.SchemaTypes.ObjectId //post or comment, message id
+    },
+    info: {
+        type: String //like count, comment, etc
+    }
+});
+
 const notifSchema = new mongoose.Schema({
     type : {
         type: String
@@ -11,10 +20,24 @@ const notifSchema = new mongoose.Schema({
         type: mongoose.SchemaTypes.ObjectId
     },
     data : {
+        type: dataSchema
+    },
+    fromType: {
         type: String
+    },
+    toType: {
+        type: String
+    },
+    action: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: mongoose.SchemaTypes.Date,
+        default: Date.now()
     }
 });
 
-const notifModel = new mongoose.model(notifModel, notifSchema);
+const notifModel = new mongoose.model("notifModel", notifSchema);
 
 export default notifModel;
--- server/routes/notifRoute.js
+++ server/routes/notifRoute.js
@@ -1,10 +1,8 @@
-import express, { request } from "express";
+import express from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
-import notifModel from "../models/notifModel";
-import userModel from "../models/userModel";
-import lspModel from "../models/lspModel";
+import notifModel from "../models/notifModel.js";
 
 const router = express.Router();
 
@@ -16,25 +14,27 @@ router.use(cors({
 router.use(cookieParser());
 
 // Types of Notifications 
-// 1) Follow request sent
-// 2) request accepted
-// 3) subscription request
-// 4) sent you message 
-// 5) payment notif
-// 6) no. of likes
-// 7) commented on commented
-// 8) commented on post
-// 9) get all the notifs
-// 10) delete notification
+// 1) Follow request sent ✔️
+// 2) request accepted ✔️
+// 3) subscription request - later
+// 4) sent you message - when chat section is made 
+// 5) payment notif ✔️
+// 6) no. of likes ✔️
+// 7) commented on commented ✔️
+// 8) commented on post ✔️
+// 9) get all the notifs ✔️
+// 10) delete notification ✔️ 
 
 router.post("/sendRequest", async(req, res) => {
     const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    const type = req.cookies.type;
     const data = req.body;
     const notifData = {
         type: "reqSent",
         from: id,
         to: data.to,
-        data: data.info
+        fromType: type,
+        toType: data.type
     };
     try{
         const notif = await notifModel.create(notifData);
@@ -57,31 +57,247 @@ router.post("/sendRequest", async(req, res) => {
 router.post("/acceptRequest", async(req, res) => {
     const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
     const type = req.cookies.type;
-    const otherid = req.body.id;
-
+    const data = req.body;
+    const notifData = {
+        type: "reqAccept",
+        from: id,
+        to: data.to,
+        fromType: type,
+        toType: data.type
+    }
     try{
-        const notifData = {
-            type: "reqAccept",
-            from: id,
-            to: otherid
-        };
-
         const notif = await notifModel.create(notifData);
 
-        if(type === "lawyer"){
-            await lspModel.updateOneById({})
+        if(notif){
+            res.status(200).json({
+                message: "notif added"
+            });
+        }
+        else{
+            res.status(205).json({
+                message: "some error"
+            });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.post("/paymentAlert", async(req, res) => {
+    const data = req.body;
+    try{
+        if(data.newConn){
+            var amount = data.newConn * 100; //amount value to be changed
+            //apply any discounts according to newConn value
+            const notif = await notifModel.create({
+                type: "paymentAlert",
+                to: data.id,
+                toType: data.type,
+                data: {
+                    info: amount
+                }
+            });
+
+            if(notif){
+                res.status(200).json({
+                    message: "Notif created"
+                });
+            }
+            else{
+                res.status(205).json({
+                    message: "some error"
+                })
+            }
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.post("/likes", async(req,res) => {
+    const data = req.body;
+    const id = data.id;
+   // const type = data.type;
+    try{
+        const notif = null;
+        if(data.typeLike === "post"){
+            notif = await notifModel.create({
+                type: "postLikes",
+                to: id,
+                toType: data.type,
+                data: {
+                    id: data.postID,
+                    info: data.likes
+                }
+            });
+        }
+        else{
+            notif = await notifModel.create({
+                type: "commLikes",
+                to: id,
+                toType: data.type,
+                data: {
+                    id: data.commID,
+                    info: data.likes
+                }
+            });
         }
 
         if(notif){
             res.status(200).json({
-                message: "Notification made"
+                message: "notif sent"
+            });
+        }
+        else{
+            res.status(205).json({
+                message: "some error"
+            });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.post("/commComm", async(req, res) => {
+    const data = req.body;
+    const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    const type = req.cookies.type;
+
+    try{
+        const notif = await notifModel.create({
+            type: "commcomm",
+            from: id,
+            to: data.id,
+            toType: data.type,
+            fromType: type,
+            data: {
+                id: data.comm
+            } 
+        });
+
+        if(notif){
+            res.status(200).json({
+                message: "notif added"
+            });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.post("/commPost", async(req, res) => {
+    const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    const type = req.cookies.type;
+    const data = req.body;
+
+    try{
+        const notif = await notifModel.create({
+            type: "postcomm",
+            from: id,
+            to: data.id,
+            toType: data.type,
+            fromType: type,
+            data: {
+                id: data.post
+            }
+        });
+
+        if(notif){
+            res.status(200).json(
+                {
+                    message: "notif added"
+                }
+            );
+        }
+        else{
+            res.status(205).json({
+                message: "some error"
+            });
+        }
+
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.get("/getAllNotifs", async(req, res) => {
+    const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    try{
+        const notifs = await notifModel.find({to: id});
+        if(notifs){
+            res.status(200).json(notifs);
+        }
+        else{
+            res.status(205).json({
+                message: "no notifications"
             })
         }
     }
     catch(error){
-
+        res.status(500).json(error);
     }
 });
 
+router.get("/getNotifs", async(req, res)=> {
+    const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    
+    try{
+        const notifs = await notifModel.find({to: id}, {action: false});
+
+        if(notifs){
+            res.status(200).json(notifs);
+        }
+        else{
+            res.status(205).json({
+                message: "some error"
+            })
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.put("/changeAction", async(req, res) => {
+    const notifID = req.body.id;
+    try{
+        const change = await notifModel.updateOne({_id: notifID}, {$set : {action: true}});
+        if(change){
+            res.status(200).json({
+                message: "notification read"
+            });
+        }
+        else{
+            res.status(205).json({
+                message: "some error"
+            });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+router.delete("/delNotifs", async(req,res) => {
+    const id = jwt.decode(req.cookies.logged, "cugelaiUzcldiufgaewiufgsldjc");
+    try{
+        const delTo = await notifModel.deleteMany({to: id});
+        const delFrom = await notifModel.deleteMany({from: id});
+
+        if(delTo && delFrom){
+            res.status(200).json({
+                message: "delete successful"
+            });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
 
 export default router;
